1b825da3932a46826101c307d4d1ddc12fb3e8c9
Tanvir <toukir.developer.bd@gmail.com>
2023-05-10 22:18:35 +0600

    added login and signUp section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 // import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
@@ -11,6 +13,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signUp",
+        element: <SignUp></SignUp>,
       },
     ],
   },
